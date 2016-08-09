9dde95d2e305ba35997e5c2a7ae25d3c36bc0991
Rahul Hariharan <rahul.hariharan2@cognizant.com>
2016-08-09 21:39:29 +0530

    shuffled list

--- js/candidate_shuffler.js
+++ js/candidate_shuffler.js
@@ -1,6 +1,48 @@
-var namelist = [];
+var namelist = [// iOS
+				"Benny",
+				"Muttiah",
+				"Ramkumar",
+                // Android
+				"Rahul",
+				"Mohan Raja",
+				"Kannan",
+				"Natraj",
+				// Web
+				"Vasanth",
+				"Mohan",
+				"Suchitra",
+				// admin ui
+				"Sri",
+				"Saikat",
+				// Service
+				"Sathya",
+				"Sreenivas",
+				// QA
+				"Ramya",
+				"Brindha",
+				"Sharath",
+				"Dinesh",
+				"Magdalene",
+				"Kevin",
+				// Moodle
+				"Selva",
+				"Makita",
+				];
 
-function populateNameList(){
 
-	
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
 }
+
+function getShuffledList(){
+
+	shuffle(namelist);
+	return namelist;
+}
+
